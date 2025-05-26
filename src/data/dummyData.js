04cc51cc22742ec24a342e9195572c8c846b2081
Simon <simonwarui93@gmail.com>
2025-05-26 17:25:35 +0300

    added dummy supplier data

---
+++ src/data/dummyData.js
@@ -0,0 +1,20 @@
+export const dummySuppliers = [
+  {
+    id: 1,
+    name: "James Sir",
+    contact: "james@gmail.com.com",
+    phone: "0700 001 001"
+  },
+  {
+    id: 2,
+    name: "Global Supply Co.",
+    contact: "global@gmail.com",
+    phone: "0798 890 900"
+  },
+  {
+    id: 3,
+    name: "LogiTrack Ltd.",
+    contact: "contact@logitrack.com",
+    phone: "0789 670 909"
+  }
+];
